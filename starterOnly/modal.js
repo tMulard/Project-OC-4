f54b1bea7ea9c7d270cd1dcbaf796278dd0f01a8
tMulard <theo.mulard@gmail.com>
2023-04-08 16:52:03 +0200

    Fixed images + edit verifycheckbox

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,16 +1,18 @@
 const RESULTS = {
   firstname: null,
   lastname: null,
-  mail:null,
+  mail: null,
   birthdate: null,
   sportquantity: null,
   location: null,
-  checkbox: null
+  cgu: null,
+  newsletter: null
 }
 
 // ====================
 //    🏗️ FONCTIONS
 // ====================
+
 // burger handler
 const openBurgerNav = () => {
   const topNav = document.getElementById("myTopnav");
@@ -89,21 +91,16 @@ const verifyMail = () => {
 
 const verifyBirthDate = () => {
   RESULTS.birthdate = null
-  
+  const today = new Date()
+  const givenDate = new Date(inputUserBirthDate.value)
+  const regexbd = /^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/gm
+
   const formDataBirthDate = document.querySelector('.formData-birthdate')
   formDataBirthDate.setAttribute('data-error-visible', false)
   
-  const regexbd = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/gm
-  
-  if (inputUserBirthDate.value.length > 9) {
-    const result = inputUserBirthDate.value.match(regexbd)
-    if(result) {
+  if (inputUserBirthDate.value.match(regexbd) && givenDate < today) {
       // ok c'est tout bon
-      RESULTS.birthdate = result.join("")
-    } else {
-      formDataBirthDate.setAttribute('data-error-visible', true)
-      formDataBirthDate.setAttribute('data-error', "Uniquement des chiffres svp.")
-    }
+      RESULTS.birthdate = inputUserBirthDate.value
   } else {
     formDataBirthDate.setAttribute('data-error-visible', true)
     formDataBirthDate.setAttribute('data-error', "Veuillez entrer votre date de naissance.")
@@ -118,9 +115,7 @@ const verifySport = () => {
     
   if (inputUserSportQuantity.value >= 0) {
     //   // ok c'est tout bon
-    const result = [];
-    result[0] = inputUserSportQuantity.value;
-    RESULTS.sportquantity = result.join("")
+    RESULTS.sportquantity = parseInt(inputUserSportQuantity.value);
   } else {
     formDataSport.setAttribute('data-error-visible', true)
     formDataSport.setAttribute('data-error', "Veuillez entrer un chiffre correct.")
@@ -131,38 +126,37 @@ const verifyLocation = () => {
   RESULTS.location = null
   const formDataLocation = document.querySelector('.formData-location')
   formDataLocation.setAttribute('data-error-visible', false)
-  
-  if ((inputUserLocation1.value != null) ||
-  (inputUserLocation2.value != null) ||
-  (inputUserLocation3.value != null) ||
-  (inputUserLocation4.value != null) ||
-  (inputUserLocation5.value != null) ||
-  (inputUserLocation6.value != null)) {
-    //   // ok c'est tout bon
-    const locationArray = [];
-    for (let i = 0; i < 6; i++) {
-      locationArray[i] = inputUserLocation${(i+1)};
-    }
-    RESULTS.location = locationArray.join(" ")
-  } else {
+
+  const citySelected = document.querySelector("input[type='radio']:checked")
+
+  if (citySelected === null) {
     formDataLocation.setAttribute('data-error-visible', true)
     formDataLocation.setAttribute('data-error', "Vous devez choisir une option.")
+  }
+  else {
+    RESULTS.location = citySelected.value
   }
 }
 
 const verifyCheckBox = () => {
-  RESULTS.checkbox = null
+  RESULTS.cgu = "off"
+  RESULTS.newsletter = "off"
   const formDataCheck = document.querySelector('.formData-checkbox')
   formDataCheck.setAttribute('data-error-visible', false)
   
-  if (inputUserCheckBox1.value == null) {
+  if (inputUserCGU.value == "off") {
    formDataCheck.setAttribute('data-error-visible', true)
    formDataCheck.setAttribute('data-error', "Vous devez vérifier que vous acceptez les termes et conditions.")
   } else {
    //   // ok c'est tout bon
-   RESULTS.checkbox = result.join("")
+   RESULTS.cgu = inputUserCGU.value
   }
-
+  if (inputUserNewsletter.value == "off") {
+    console.log("Êtes-vous sûr(e) de ne pas vouloir être abonné(e) chez nous?")
+   } else {
+    //   // ok c'est tout bon
+    RESULTS.newsletter = inputUserNewsletter.value
+   }
 }
 
 const toggleModal = () => {
@@ -183,16 +177,8 @@ const inputUserLastName = document.querySelector("#last")
 const inputUserMail = document.querySelector("#email")
 const inputUserBirthDate = document.querySelector("#birthdate")
 const inputUserSportQuantity = document.querySelector("#quantity")
-
-const inputUserLocation1 = document.querySelector("#location1")
-const inputUserLocation2 = document.querySelector("#location2")
-const inputUserLocation3 = document.querySelector("#location3")
-const inputUserLocation4 = document.querySelector("#location4")
-const inputUserLocation5 = document.querySelector("#location5")
-const inputUserLocation6 = document.querySelector("#location6")
-
-const inputUserCheckBox1 = document.querySelector("#checkbox1")
-const inputUserCheckBox2 = document.querySelector("#checkbox2")
+const inputUserCGU = document.querySelector("#checkbox1")
+const inputUserNewsletter = document.querySelector("#checkbox2")
 
 const button = document.querySelector('.btn-submit')
 
@@ -216,6 +202,7 @@ button.addEventListener('click', (event) => {
   verifyCheckBox()
 
   console.log(RESULTS);
+
   if (
      RESULTS.firstname &&
      RESULTS.lastname &&
@@ -223,10 +210,13 @@ button.addEventListener('click', (event) => {
      RESULTS.birthdate &&
      RESULTS.sportquantity &&
      RESULTS.location &&
-     RESULTS.checkbox 
+     RESULTS.cgu
    ) {
    // si tout est ok on affiche le message
   console.log("Merci ! Votre réservation a été reçue.");
+  } else {
+    // message d'erreur
+    console.log("Erreur, les champs n'ont pas été remplis correctement")
   }
   
 })
